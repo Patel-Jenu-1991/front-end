5f25b0e4e6a814b1de5eafcf17e6278f53295553
Jenu Patel <jenu.16@icloud.com>
2017-06-27 16:40:31 -0500

    Completed the todo app

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 $(function() {
     console.log("whee!")
+
+    // event handler
+    $("#btn-click").click(function() {
+        if ($('input').val() !== '') {
+            // grab the value from the input box after the button click
+            var input = $("input").val()
+            // display value within the browser's JS console
+            console.log(input)
+            // add the value to the DOM
+            $('ol').append('<li><a href="">x - </a>'+ input + '</li>');
+        }
+        $('input').val('');
+    });
+
+    // remove todos
+    $(document).on('click', 'a', function(event) {
+        event.preventDefault();
+        $(this).parent().remove();
+    });
 });
